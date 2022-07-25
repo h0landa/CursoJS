e46772abc67ab89a4ad05a166e95ae8b76a930db
Arthur Holanda <holandaarthur46@gmail.com>
2022-07-25 08:12:19 -0300

    Super Contador

--- aula14/exercicio 01/script.js
+++ aula14/exercicio 01/script.js
@@ -1,7 +1,18 @@
 c = 1
+var resultado = document.getElementById('res')
 function contar(){
+    resultado.innerText = ''
     var fim = Number(document.getElementById('fim').value)
-    for (var ini = Number(document.getElementById('inicio').value); ini <= fim; ini++){
-        console.log(ini)
+    var passo = Number(document.getElementById('passo').value)
+    var ini = Number(document.getElementById('inicio').value)
+    if (ini > fim){
+        resultado.style.color = 'red'
+        resultado.innerText = 'IMPOSSIVEL CONTAR'
+    }
+    else{
+        for (ini; ini <= fim; ini+=passo){
+            resultado.innerText += `${ini} 👉`
+        }
+        resultado.innerText += '🏁'
     }
 }
